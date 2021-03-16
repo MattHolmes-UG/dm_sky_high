1640b6ab91a63dc528e9987f2ecc5d958fa7b9a4
MattHolmes <matthewochonogorug@gmail.com>
2021-03-16 17:15:42 +0100

    Updates

---
+++ precache-manifest.d3c1b13adeaeacb84d720ce6630b1d6c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae3e0c4327086258999d2f1f0f4f19b",
+    "url": "/dm_sky_high/index.html"
+  },
+  {
+    "revision": "2ae704b431eea4b60381",
+    "url": "/dm_sky_high/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "cefb8ca594d53c535123",
+    "url": "/dm_sky_high/static/css/main.2d60bd9e.chunk.css"
+  },
+  {
+    "revision": "2ae704b431eea4b60381",
+    "url": "/dm_sky_high/static/js/2.f187a73c.chunk.js"
+  },
+  {
+    "revision": "25724449fa7b7df3da3572d055e0b556",
+    "url": "/dm_sky_high/static/js/2.f187a73c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cefb8ca594d53c535123",
+    "url": "/dm_sky_high/static/js/main.7ab54a67.chunk.js"
+  },
+  {
+    "revision": "b605231705887f4ba60f",
+    "url": "/dm_sky_high/static/js/runtime-main.35cc7c70.js"
+  },
+  {
+    "revision": "48b6c0374ed29a441a870a98374a2433",
+    "url": "/dm_sky_high/static/media/pexels-photo-723240.48b6c037.jpeg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/dm_sky_high/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/dm_sky_high/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
